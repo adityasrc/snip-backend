6323d5489786ea1eb36ac4cd1f1af1b2d1a40f5e
Aditya Prakash <adityaprakash.bih@gmail.com>
2026-03-09 22:29:45 +0530

    chore: final touch for deployment

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ import authRoutes from "./routes/auth.js";
 import linkRoutes from "./routes/links.js";
 import redirectRoutes from "./routes/redirect.js";
 
+const PORT = process.env.PORT || 10000;
+
 const app = express();
 
 app.use(express.json()); // to parse the json body
 app.use(cors());
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/links", linkRoutes);
@@ -21,19 +22,21 @@ app.use("/", redirectRoutes); // The short link redirector
 // Global Error Handler (SDE-1 Flex)
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: "Internal Server Error! Something went wrong." });
+  res
+    .status(500)
+    .json({ message: "Internal Server Error! Something went wrong." });
 });
 
 async function main() {
-    try {
-        await mongoose.connect(process.env.MD_URL);
-        console.log("Connected to MongoDB");
-        app.listen(3000, () => {
-            console.log("Server running at port 3000");
-        });
-    } catch (e) {
-        console.error("DB Connection Failed:", e.message);
-    }
+  try {
+    await mongoose.connect(process.env.MD_URL);
+    console.log("Connected to MongoDB");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server running at port ${PORT}`);
+    });
+  } catch (e) {
+    console.error("DB Connection Failed:", e.message);
+  }
 }
 
 main();
